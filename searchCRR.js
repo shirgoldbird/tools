7dbbbe44dca316225bc373d3d1ce34d0e725fadb
JiayinPei <jipe@microsoft.com>
2019-09-04 11:35:43 +0800

    searchCRR

---
+++ searchCRR.js
@@ -0,0 +1,33 @@
+const fs = require('fs-extra')
+const program = require('commander')
+
+const targets = [
+    'https://github.com/azure/ocp-azure-quick-start',
+    'https://github.com/azure/batchsparkscoringpredictiveaaintenance',
+    'https://github.com/azure/azure-cosmos-dotnet-v3',
+    'https://github.com/azure/azure-spatial-anchors-samples'
+]
+const { searchFromFolders } = require('./utils/search_util')
+
+program.option('-p --basePath <basePath>', 'search folder path');
+program.parse(process.argv);
+searchFromFolders(program.basePath, searchMarkdowns, searchCRR);
+
+function searchMarkdowns(searchPath) {
+    return [`${searchPath}/.openpublishing.publish.config.json`];
+}
+
+async function searchCRR(file) {
+    const config = fs.readJSONSync(file, { encoding: 'utf-8' });
+    var targetFound = false;
+    if (config.dependent_repositories) {
+        for (var i = 0; i < config.dependent_repositories.length; i++) {
+            var crr = config.dependent_repositories[i];
+            if (targets.indexOf(crr.url.toLowerCase()) >= 0) {
+                console.log(`Found CRR ${crr.url}`);
+                targetFound = true;
+            }
+        }
+    }
+    return targetFound;
+}
